de8efce54dedc1ffeb6911172b33bb1ce60a4489
Mainerprenuer <moustymainer@gmail.com>
2024-07-19 16:02:53 +0100

    shadcn config

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-white text-3xl underline">Home</h1>
+    <div className="flex h-screen max-h-screen"> 
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          {/* <Image /> */}
+        </div>
+      </section>
     </div>
   );
 }
